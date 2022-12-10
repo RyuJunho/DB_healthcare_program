6c55d0bc4fbf9824503e63ee97bd65f2b7063571
GaYoung Jung <mini3100@naver.com>
2022-12-10 19:23:31 +0900

    mainUI JTable 추가

--- db_project/src/UI/mainUI.java
+++ db_project/src/UI/mainUI.java
@@ -18,6 +18,14 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumnModel;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.LineBorder;
+import javax.swing.table.TableModel;
 public class mainUI extends JFrame {
 	DB_Conn_Query db = new DB_Conn_Query();
 	JComboBox checkUpBox;
@@ -28,6 +36,8 @@ public class mainUI extends JFrame {
 	String person_number;		// 주민등록번호
 	String sex;				// 성별
 	float avg;
+	private JTable dataTable;
+	private DefaultTableModel model;
 	
 	public mainUI(String id) {
 		setTitle("Main");
@@ -35,7 +45,7 @@ public class mainUI extends JFrame {
 		
 		JPanel userInfoPanel = new JPanel();
 		userInfoPanel.setBackground(SystemColor.inactiveCaptionBorder);
-		userInfoPanel.setBounds(35, 96, 408, 483);
+		userInfoPanel.setBounds(25, 96, 300, 360);
 		getContentPane().add(userInfoPanel);
 		userInfoPanel.setLayout(null);
 		
@@ -84,7 +94,7 @@ public class mainUI extends JFrame {
 		//--------------------------회원 정보 가져오기 end---------------------------
 		JPanel chartPanel = new JPanel();
 		chartPanel.setBackground(SystemColor.inactiveCaptionBorder);
-		chartPanel.setBounds(455, 96, 700, 483);
+		chartPanel.setBounds(337, 96, 532, 360);
 		getContentPane().add(chartPanel);
 		chartPanel.setLayout(null);
 		
@@ -93,12 +103,12 @@ public class mainUI extends JFrame {
 		JLabel avgValueLabel = new JLabel("0");
 		avgValueLabel.setHorizontalAlignment(SwingConstants.RIGHT);
 		avgValueLabel.setFont(new Font("맑은 고딕", Font.PLAIN, 12));
-		avgValueLabel.setBounds(536, 451, 127, 22);
+		avgValueLabel.setBounds(355, 311, 160, 22);
 		chartPanel.add(avgValueLabel);
 		
 		JLabel avgLabel = new JLabel("수치 평균");
 		avgLabel.setFont(new Font("맑은 고딕", Font.PLAIN, 12));
-		avgLabel.setBounds(536, 421, 127, 29);
+		avgLabel.setBounds(355, 281, 127, 29);
 		chartPanel.add(avgLabel);
 		
 		//-------------------------콤보박스 데이터 가져오기--------------------------------
@@ -115,6 +125,7 @@ public class mainUI extends JFrame {
 				//검진항목이 선택되면 그에 따른 질환명 데이터 콤보박스 출력
 				String[] diseaseData = combo.getData("질환명",checkUpBox.getSelectedItem().toString());
 				diseaseBox.setModel(new DefaultComboBoxModel(diseaseData));
+				diseaseBox.setSelectedIndex(0);
 				//검진항목 바뀌었을 때 조사항목 콤보박스 비우기
 				examinationBox.removeAllItems();
 			}
@@ -125,11 +136,13 @@ public class mainUI extends JFrame {
 				//질환명이 선택되면 그에 따른 조사항목 데이터 콤보박스 출력
 				String[] examinationData = combo.getData("조사항목",diseaseBox.getSelectedItem().toString());
 				examinationBox.setModel(new DefaultComboBoxModel(examinationData));
+				examinationBox.setSelectedIndex(0);
 			}
 		});
 		
 		examinationBox.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				model.setNumRows(0);
 				//하나라도 선택되지 않으면 수치 평균 출력 x
 				if(!(checkUpBox.getSelectedItem()==null||diseaseBox.getSelectedItem()==null||examinationBox.getSelectedItem()==null)) {
 					//조사항목이 선택되면 차트 업데이트, 수치 평균 출력
@@ -151,21 +164,42 @@ public class mainUI extends JFrame {
 					} catch (SQLException e1) {
 						e1.printStackTrace();
 					}
+					
+					//JTable 행 추가
+					String sql2 = "SELECT 검사날짜, 수치값, 이상유무 "
+							+ "FROM 회원수치 "
+							+ "WHERE ID = "+id+" AND "
+							+ "조사항목 = '"+examination+"'";
+					ResultSet rs = db.executeQuery(sql2);
+					try {
+						int i=0,j=0;
+						while(rs.next()) {
+							String date = rs.getDate(1).toString();
+							String value = Float.toString(rs.getFloat(2));
+							String tf = rs.getString(3);
+							//TableCellRenderer render = dataTable.getCellRenderer(i++, j++);
+							//render.setForeground(Color.red);
+							model.addRow(new Object[] {date,value});
+						}
+					} catch (SQLException e1) {
+						e1.printStackTrace();
+					}
 				}
 				else {
-					avgValueLabel.setText("");
+					avgValueLabel.setText("0");
+					model.setNumRows(0);
 				}
 			}
 		});
 		
 		checkUpBox.setFont(new Font("맑은 고딕", Font.PLAIN, 12));
-		checkUpBox.setBounds(20, 451, 160, 22);
+		checkUpBox.setBounds(355, 74, 160, 22);
 		
 		diseaseBox.setFont(new Font("맑은 고딕", Font.PLAIN, 12));
-		diseaseBox.setBounds(192, 451, 160, 22);
+		diseaseBox.setBounds(355, 150, 160, 22);
 		
 		examinationBox.setFont(new Font("맑은 고딕", Font.PLAIN, 12));
-		examinationBox.setBounds(364, 451, 160, 22);
+		examinationBox.setBounds(355, 230, 160, 22);
 		
 		chartPanel.add(checkUpBox);
 		chartPanel.add(diseaseBox);
@@ -174,32 +208,57 @@ public class mainUI extends JFrame {
 		
 		JLabel checkUpLabel = new JLabel("검진 항목");
 		checkUpLabel.setFont(new Font("맑은 고딕", Font.PLAIN, 12));
-		checkUpLabel.setBounds(20, 421, 127, 29);
+		checkUpLabel.setBounds(355, 44, 127, 29);
 		chartPanel.add(checkUpLabel);
 		
 		JLabel diseaseLabel = new JLabel("질환");
 		diseaseLabel.setFont(new Font("맑은 고딕", Font.PLAIN, 12));
-		diseaseLabel.setBounds(192, 421, 127, 29);
+		diseaseLabel.setBounds(355, 120, 127, 29);
 		chartPanel.add(diseaseLabel);
 		
 		JLabel examinationLabel = new JLabel("조사 항목");
 		examinationLabel.setFont(new Font("맑은 고딕", Font.PLAIN, 12));
-		examinationLabel.setBounds(364, 421, 127, 29);
+		examinationLabel.setBounds(355, 200, 127, 29);
 		chartPanel.add(examinationLabel);
+		
+		JLabel lblNewLabel_4 = new JLabel("날짜별 수치 기록");
+		lblNewLabel_4.setFont(new Font("맑은 고딕", Font.PLAIN, 13));
+		lblNewLabel_4.setBounds(22, 10, 188, 24);
+		chartPanel.add(lblNewLabel_4);
+		
+		//------------------------- 회원 수치 테이블 --------------------------
+		String colName[] = {"날짜","수치값"};
+		model = new DefaultTableModel(colName,0);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(22, 44, 310, 289);
+		chartPanel.add(panel);
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		JScrollPane scrollPane = new JScrollPane();
+		panel.add(scrollPane, BorderLayout.CENTER);
+		
+		dataTable = new JTable(model);
+		dataTable.setBorder(new LineBorder(new Color(0, 0, 0)));
+		scrollPane.setViewportView(dataTable);
+		DefaultTableCellRenderer dtcr = new DefaultTableCellRenderer();
+		dtcr.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		//------------------------ 회원 수치 테이블 end ---------------------
 		
 		JLabel lblNewLabel = new JLabel("회원 정보");
 		lblNewLabel.setFont(new Font("맑은 고딕", Font.PLAIN, 15));
-		lblNewLabel.setBounds(35, 59, 92, 36);
+		lblNewLabel.setBounds(25, 59, 92, 36);
 		getContentPane().add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("회원 수치값");
 		lblNewLabel_1.setFont(new Font("맑은 고딕", Font.PLAIN, 15));
-		lblNewLabel_1.setBounds(455, 59, 138, 36);
+		lblNewLabel_1.setBounds(337, 59, 138, 36);
 		getContentPane().add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("건강 관리 프로그램");
 		lblNewLabel_2.setFont(new Font("맑은 고딕", Font.BOLD, 25));
-		lblNewLabel_2.setBounds(35, 10, 280, 52);
+		lblNewLabel_2.setBounds(25, 10, 280, 52);
 		getContentPane().add(lblNewLabel_2);
 		//--------------------------회원 탈퇴 이벤트----------------------------------
 		JButton dropBtn = new JButton("회원 탈퇴");
@@ -218,7 +277,7 @@ public class mainUI extends JFrame {
 		});
 		//----------------------------------------------------------------------
 		dropBtn.setFont(new Font("맑은 고딕", Font.PLAIN, 12));
-		dropBtn.setBounds(932, 25, 107, 31);
+		dropBtn.setBounds(643, 30, 107, 31);
 		getContentPane().add(dropBtn);
 		
 		JButton dataBtn = new JButton("건강검진 데이터 관리");
@@ -229,7 +288,7 @@ public class mainUI extends JFrame {
 			}
 		});
 		dataBtn.setFont(new Font("맑은 고딕", Font.PLAIN, 12));
-		dataBtn.setBounds(35, 608, 177, 31);
+		dataBtn.setBounds(25, 466, 177, 31);
 		getContentPane().add(dataBtn);
 		
 		JButton logoutBtn = new JButton("로그아웃");
@@ -241,11 +300,11 @@ public class mainUI extends JFrame {
 			}
 		});
 		logoutBtn.setFont(new Font("맑은 고딕", Font.PLAIN, 12));
-		logoutBtn.setBounds(1051, 26, 107, 31);
+		logoutBtn.setBounds(762, 31, 107, 31);
 		getContentPane().add(logoutBtn);
 		
 		//화면 설정
-		setSize(1200,700);
+		setSize(920,550);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	//닫기 누르면 종료
 		setLocationRelativeTo(null);	//화면 중앙 배치
 		setResizable(false);			// 화면 사이즈 고정
